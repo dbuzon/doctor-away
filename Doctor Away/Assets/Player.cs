326d1aa9176b55c8bb3dd3487ee3b3924bacbfb7
Debora Buzon <debora.buzon@gmail.com>
2022-12-01 00:01:12 -0300

    Add player's health UI

--- Doctor Away/Assets/Player.cs
+++ Doctor Away/Assets/Player.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
     public int health = 5;
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
     //public GameObject deathEffect;
 
     void OnCollisionEnter2D(Collision2D collision) {
         health--;
+        hearts[health].sprite = emptyHeart; 
+
         if (health <= 0) {
             Die();
         }
