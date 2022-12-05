3dedc4e7455fb55c7ed2b912a3a55545d5d878a2
Debora Buzon <debora.buzon@gmail.com>
2022-12-05 20:01:52 -0300

    Fix bullet sound effect

--- Doctor Away/Assets/Shooting.cs
+++ Doctor Away/Assets/Shooting.cs
@@ -7,6 +7,12 @@ public class Shooting : MonoBehaviour {
     public GameObject bulletObj;
     public GameObject shotEffect;
     public float bulletForce = 20f;
+    AudioSource audioSource;
+    public AudioClip gunshot;
+
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     void Update() {
@@ -19,6 +25,7 @@ public class Shooting : MonoBehaviour {
         GameObject effect = Instantiate(shotEffect, firePoint.position, firePoint.rotation);
         GameObject bullet = Instantiate(bulletObj, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        audioSource.PlayOneShot(gunshot, 0.7F);
         rb.AddForce(firePoint.right * bulletForce, ForceMode2D.Impulse);
         Destroy(effect, 0.2f);
     }
